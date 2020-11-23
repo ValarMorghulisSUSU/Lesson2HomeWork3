aab58588dcba6f75ea3a55a508f2179209f8fb73
Ekaterina Polupanova <lenovoa736@gmail.com>
2020-11-23 17:06:48 +0300

    Initial Commit

---
+++ Lesson2HomeWork3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson2HomeWork3
+//
+//  Created by Admin on 23.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
